26c5206c45eff1019840571d7671b3355b76243b
n4to4 <n4to4k@gmail.com>
2022-04-06 20:55:49 +0900

    ConcurrencyLimit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::{
     convert::Infallible,
-    future::{ready, Future, Ready},
-    pin::Pin,
+    future::{ready, Ready},
     sync::Arc,
     task::{Context, Poll},
     time::Duration,
 };
 
+use futures::future::BoxFuture;
 use hyper::{server::conn::AddrStream, service::Service, Body, Request, Response, Server};
 use tokio::sync::{OwnedSemaphorePermit, Semaphore};
 use tokio_util::sync::PollSemaphore;
+use tower::limit::ConcurrencyLimit;
 
 #[tokio::main]
 async fn main() {
@@ -24,7 +25,7 @@ const MAX_INFLIGHT_REQUESTS: usize = 5;
 
 struct MyServiceFactory {
     conn_semaphore: PollSemaphore,
-    reqs_semaphore: PollSemaphore,
+    reqs_semaphore: Arc<Semaphore>,
     permit: Option<OwnedSemaphorePermit>,
 }
 
@@ -32,14 +33,14 @@ impl Default for MyServiceFactory {
     fn default() -> Self {
         Self {
             conn_semaphore: PollSemaphore::new(Arc::new(Semaphore::new(MAX_CONNS))),
-            reqs_semaphore: PollSemaphore::new(Arc::new(Semaphore::new(MAX_INFLIGHT_REQUESTS))),
+            reqs_semaphore: Arc::new(Semaphore::new(MAX_INFLIGHT_REQUESTS)),
             permit: None,
         }
     }
 }
 
 impl Service<&AddrStream> for MyServiceFactory {
-    type Response = MyService;
+    type Response = ConcurrencyLimit<MyService>;
     type Error = Infallible;
     type Future = Ready<Result<Self::Response, Self::Error>>;
 
@@ -58,39 +59,31 @@ impl Service<&AddrStream> for MyServiceFactory {
             "↑ {} connections",
             MAX_CONNS - self.conn_semaphore.available_permits()
         );
-        ready(Ok(MyService {
-            _permit: permit,
-            semaphore: self.reqs_semaphore.clone(),
-            reqs_permit: None,
-        }))
+        ready(Ok(ConcurrencyLimit::with_semaphore(
+            MyService {
+                _conn_permit: permit,
+            },
+            self.reqs_semaphore.clone(),
+        )))
     }
 }
 
 struct MyService {
-    _permit: OwnedSemaphorePermit,
-    semaphore: PollSemaphore,
-    reqs_permit: Option<OwnedSemaphorePermit>,
+    _conn_permit: OwnedSemaphorePermit,
 }
 
 impl Service<Request<Body>> for MyService {
     type Response = Response<Body>;
     type Error = Infallible;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        if self.reqs_permit.is_none() {
-            self.reqs_permit = Some(futures::ready!(self.semaphore.poll_acquire(cx)).unwrap());
-        }
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Ok(()).into()
     }
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
-        let permit = self.reqs_permit.take().expect(
-            "you didn't drive me to readiness did you? you know that's a tower crime right?",
-        );
         println!("{} {}", req.method(), req.uri());
         Box::pin(async move {
-            let _permit = permit;
             tokio::time::sleep(Duration::from_millis(250)).await;
             Ok(Response::builder().body("Hello World\n".into()).unwrap())
         })
