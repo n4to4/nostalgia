a7cb7aaae88d9477b2436c24831f80027f3fa473
n4to4 <n4to4k@gmail.com>
2022-04-08 22:07:02 +0900

    No sleep

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
-use std::{convert::Infallible, sync::Arc, time::Duration};
+use std::convert::Infallible;
 
-use hyper::{server::conn::AddrStream, service::make_service_fn, Body, Request, Response, Server};
-use tokio::sync::Semaphore;
-use tower::limit::GlobalConcurrencyLimitLayer;
-use tower::ServiceBuilder;
-
-const MAX_CONNS: usize = 50;
+use hyper::{
+    server::conn::AddrStream, service::make_service_fn, service::service_fn, Body, Request,
+    Response, Server,
+};
 
 #[tokio::main]
 async fn main() {
-    let conns_limit = Arc::new(Semaphore::new(MAX_CONNS));
-    let app = make_service_fn(move |_stream: &AddrStream| {
-        let conns_limit = conns_limit.clone();
-        async move {
-            let permit = Arc::new(conns_limit.acquire_owned().await.unwrap());
-            Ok::<_, Infallible>(
-                ServiceBuilder::new()
-                    .then(|res: Result<Response<Body>, Infallible>| {
-                        drop(permit);
-                        std::future::ready(res)
-                    })
-                    .service_fn(hello_world),
-            )
-        }
+    let app = make_service_fn(move |_stream: &AddrStream| async move {
+        Ok::<_, Infallible>(service_fn(hello_world))
     });
 
     Server::bind(&([127, 0, 0, 1], 1025).into())
@@ -33,7 +19,6 @@ async fn main() {
 
 async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     println!("{} {}", req.method(), req.uri());
-    tokio::time::sleep(Duration::from_millis(250)).await;
     Ok(Response::builder()
         .body(Body::from("Hello World!\n"))
         .unwrap())
